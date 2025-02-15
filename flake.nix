5b754f6a1f4932775042990ee417f35099598e66
Jack Dewinter <jackdewinter1@gmail.com>
2025-02-15 16:12:54 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Nix System Configuration for Jack";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    nixpkgs,
+    darwin,
+    home-manager,
+    nixos-wsl,
+    helix-master,
+    ...
+  } @ inputs: let
+    darwinSystem = {user, arch ? "aarch64-darwin"}:
+      darwin.lib.darwinSystem {
+        system = arch;
+        modules = [
+          ./darwin/darwin.nix
+          home-manager.darwinModules.home-manager
+          {
+            _module.args = { inherit inputs; };
+            home-manager = {
+              users.${user} = import ./home-manager;
+            };
+            users.users.${user}.home = "/Users/${user}";
+            nix.settings.trusted-users = [ user ];
+          }
+        ];
+      };
+  in
+  {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.wsl
+          ./nixos/configuration.nix
+          ./.config/wsl
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              users.nixos = import ./home-manager;
+            };
+            nix.settings.trusted-users = [ "nixos" ];
+          }
+        ];
+      };
+    };
+    darwinConfigurations = {
+      "Jacks-Mac-Studio" = darwinSystem {
+        user = "jackdewinter";
+      };
+      "Jacks-MacBook-Air" = darwinSystem {
+        user = "jackdewinter";
+      };
+    };
+  };
+}
