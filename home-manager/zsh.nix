17740de1693539b12704de32d48307d1b7097ded
Jack Dewinter <jackdewinter1@gmail.com>
2025-04-05 17:50:15 +0100

    use full zsh config

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -9,10 +9,15 @@
     enable = true;
     oh-my-zsh = {
     	enable = true;
+			plugins = [ "git" ];
+			theme = "agnoster";
     };
     shellAliases = {
     	ll = "ls -l";
     	update = "darwin-rebuild switch --flake ~/code/dotfiles-nix";
     };
+		initExtra = ''
+			. ${../.config/zsh/.zshrc}
+		'';
   };
 }
